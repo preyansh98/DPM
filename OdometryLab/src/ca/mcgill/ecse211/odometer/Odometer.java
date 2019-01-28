3681d430dc169d14178ee9d60f47a87645a48d5c
maximerieuf <44277441+maximerieuf@users.noreply.github.com>
2019-01-28 17:23:56 -0500

    Update Odometer.java

--- OdometryLab/src/ca/mcgill/ecse211/odometer/Odometer.java
+++ OdometryLab/src/ca/mcgill/ecse211/odometer/Odometer.java
@@ -18,8 +18,10 @@ public class Odometer extends OdometerData implements Runnable {
   private static Odometer odo = null; // Returned as singleton
 
   // Motors and related variables
-  private int leftMotorTachoCount;
-  private int rightMotorTachoCount;
+  
+  //The tacho counts refer to the rotations of the respective motors. 
+  private int leftMotorTachoCount; //
+  private int rightMotorTachoCount; //
   private EV3LargeRegulatedMotor leftMotor;
   private EV3LargeRegulatedMotor rightMotor;
 
@@ -27,7 +29,7 @@ public class Odometer extends OdometerData implements Runnable {
   private final double WHEEL_RAD;
 
   
-  double ThetaVal = 0; //do we want to initialize or store this var?
+  double ThetaVal = 0; //A global variable for later to store the theta value calculated
   
 
 
@@ -51,8 +53,8 @@ public class Odometer extends OdometerData implements Runnable {
     // Reset the values of x, y and z to 0
     odoData.setXYT(0, 0, 0);
 
-    this.leftMotorTachoCount = 0;
-    this.rightMotorTachoCount = 0;
+    this.leftMotorTachoCount = 0; //initialized the current leftMotorTachoCount to 0
+    this.rightMotorTachoCount = 0; //initialized the current rightMotorTachoCount to 0
 
     this.TRACK = TRACK;
     this.WHEEL_RAD = WHEEL_RAD;
@@ -96,6 +98,12 @@ public class Odometer extends OdometerData implements Runnable {
   /**
    * This method is where the logic for the odometer will run. Use the methods provided from the
    * OdometerData class to implement the odometer.
+   * The robot knows in which direction it is going based on the number of tacho counts for the left motor and right motor.
+   * It stores these tacho counts and on each iteration compares it to the old tacho count. 
+   * 
+   * The left distance, right distance, is calculated based on the difference of these tacho counts. 
+   * The overall change in distance and change in the angle, theta, is calculated. This is then used to
+   * calculate the specific changes in the X and Y direction using trigonometry. 
    */
   // run method (required for Thread)
   public void run() {
@@ -103,34 +111,38 @@ public class Odometer extends OdometerData implements Runnable {
 
     while (true) {
       updateStart = System.currentTimeMillis();
-      //initialize
+      //initialize and store the current left and right tacho counts 
       int newLeftTacho = leftMotor.getTachoCount(); 
       int newRightTacho = rightMotor.getTachoCount(); 
 
       
-      // TODO Calculate new robot position based on tachometer counts  
-      //new x, new y, and new theta compared to last tacho count. 
+        //new x, new y, and new theta compared to last tacho count. 
       // the following doubles create the distance covered by both wheels for every rotation   
-     double leftDist = Math.PI * WHEEL_RAD * (newLeftTacho - leftMotorTachoCount)/180;
+      // the distances are calculated by multiplying the circumference (2*pi*r) by the difference in the tachocounts between the left and right motors, divided by 360 degrees. 
+     double leftDist = Math.PI * WHEEL_RAD * (newLeftTacho - leftMotorTachoCount)/180; 
      double rightDist = Math.PI * WHEEL_RAD * (newRightTacho - rightMotorTachoCount)/180; 
      
-     // updates old tachocounts to be new ones
+     
+     // updates old tachocounts to be new ones so the next iteration uses the new values
      this.leftMotorTachoCount = newLeftTacho;
      this.rightMotorTachoCount = newRightTacho;
      
      
-     double deltaDist = 0.5*(leftDist + rightDist); 
-     double deltaTheta = (leftDist - rightDist)/(TRACK); //is track wheel base?
+     double deltaDist = 0.5*(leftDist + rightDist); //average of the two distances 
+     double deltaTheta = (leftDist - rightDist)/(TRACK); //differences between distances covered by both wheels, divided by the distance between the wheels
      
 
      ThetaVal  += deltaTheta; 
-     
-     double changeInX = deltaDist * Math.sin(ThetaVal); //sin and cos are for negative values when turns back
+     //sin and cos take care of the direction and sign of the X and Y values respectively.
+     //For example when theta is 180 degrees, sin(180)=0, cos(180)=-1 so the Y value will decrease as the robot advances and the X value will not change.
+     double changeInX = deltaDist * Math.sin(ThetaVal); 
      double changeInY = deltaDist * Math.cos(ThetaVal); 
      
       
-      // TODO Update odometer values with new calculated values
-      odo.update(changeInX, changeInY, Math.toDegrees(deltaTheta)); //adds the values to the current value
+      //updates the odometer with the new X and Y values as well as the new theta converted in degrees
+     //the update method automatically adds the change to the original values
+      odo.update(changeInX, changeInY, Math.toDegrees(deltaTheta)); //deltaTheta was converted to degrees since the calculations are in radians but display and correction in degrees 
+       
 
       // this ensures that the odometer only runs once every period
       updateEnd = System.currentTimeMillis();
