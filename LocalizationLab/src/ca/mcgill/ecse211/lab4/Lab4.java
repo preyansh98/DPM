187e1fcab658894ae9404164067b94cb38f3ccbf
Preyansh <preyansh98@gmail.com>
2019-02-10 17:22:04 -0500

    set up UltrasonicLocalizer

--- LocalizationLab/src/ca/mcgill/ecse211/lab4/Lab4.java
+++ LocalizationLab/src/ca/mcgill/ecse211/lab4/Lab4.java
@@ -1,5 +1,93 @@
 package ca.mcgill.ecse211.lab4;
 
-public class Lab4 {
+import ca.mcgill.ecse211.odometer.*;
+import lejos.hardware.Button;
+import lejos.hardware.Sound;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.lcd.TextLCD;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3ColorSensor;
 
-}
+
+/**
+ * This is the main class Lab3 that initializes all sensors and passes arguments to constructors of
+ * other classes. 
+ * @author Preyansh & Maxime
+ *
+ */
+public class Lab4 {
+	
+	  // Motor Objects, and Robot related parameters
+	  private static final EV3LargeRegulatedMotor leftMotor =
+	      new EV3LargeRegulatedMotor(LocalEV3.get().getPort("B"));
+	  private static final EV3LargeRegulatedMotor rightMotor =
+	      new EV3LargeRegulatedMotor(LocalEV3.get().getPort("C"));
+	  private static final TextLCD lcd = LocalEV3.get().getTextLCD(); 
+	  public static final double WHEEL_RAD = 2.19; //2.19 set as the radius of the wheel
+	  public static final double TRACK = 10.7; //The track value refers to distance between the two wheels.
+
+	
+	/**
+	 * The main method initializes all the threads and depending on user input on 
+	 * button pressed, runs the required navigation thread. 				
+	 * @param args
+	 * @throws OdometerExceptions
+	 */
+	public static void main(String[] args) throws OdometerExceptions{
+		   int buttonChoice;
+		    // Odometer related objects
+		    Odometer odometer = Odometer.getOdometer(leftMotor, rightMotor, TRACK, WHEEL_RAD); 
+		    Display odometryDisplay = new Display(lcd); // No need to change
+		    
+		    //Objects of the navigation and obstacle avoider class are created here
+		    //We pass in the motors, track, wheel_rad and the waypoints for the map chosen
+			UltrasonicLocalizer usLocalizer; 
+			LightLocalizer lightLocalizer; 
+			
+			
+			do {
+		      // clear the displays
+		      lcd.clear();
+
+	          lcd.drawString("< Left  | Right >", 0, 0);
+	          lcd.drawString("Falling | Rising ", 0, 1);
+	          lcd.drawString(" edge   |  edge  ", 0, 2);
+	          lcd.drawString("        |        ", 0, 3);
+	          lcd.drawString("        |        ", 0, 4);
+
+	        buttonChoice = Button.waitForAnyPress();
+	      }
+    
+	    while (buttonChoice != Button.ID_LEFT && buttonChoice != Button.ID_RIGHT);		      
+		
+		    //This method starts the threads for the odometer and display
+		    initThreads(odometer, odometryDisplay); 
+		    
+		    if(buttonChoice == Button.ID_LEFT) {	
+		    	//starts the ultrasonic localizer class with falling edge
+		    	
+		    }
+		    else if(buttonChoice == Button.ID_RIGHT) {
+		    	//starts the ultrasonic localizer class with rising edge
+		    	
+		    }
+
+		    while (Button.waitForAnyPress() != Button.ID_ESCAPE);
+		    System.exit(0);
+		  }
+
+	/**
+	 * Simple method that initializes the threads for the odometer and odometer display
+	 * @param odometer
+	 * @param odometryDisplay
+	 */
+	private static void initThreads(Odometer odometer, Display odometryDisplay) {
+	    Thread odoThread = new Thread(odometer);
+	      odoThread.start();
+	      Thread odoDisplayThread = new Thread(odometryDisplay);
+	      odoDisplayThread.start();
+	   
+	}
+	
+	}
--- LocalizationLab/src/ca/mcgill/ecse211/lab4/UltrasonicLocalizer.java
+++ LocalizationLab/src/ca/mcgill/ecse211/lab4/UltrasonicLocalizer.java
@@ -1,5 +1,87 @@
 package ca.mcgill.ecse211.lab4;
 
-public class UltrasonicLocalizer {
+import ca.mcgill.ecse211.odometer.Odometer;
+import ca.mcgill.ecse211.odometer.OdometerData;
+import ca.mcgill.ecse211.odometer.OdometerExceptions;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.sensor.SensorModes;
+import lejos.robotics.SampleProvider;
 
+public class UltrasonicLocalizer extends Thread{
+	
+	//Setting up motors
+		private EV3LargeRegulatedMotor leftMotor;
+		private EV3LargeRegulatedMotor rightMotor;
+		
+		//Ultrasonic sensor initializers
+		private float[] usData;
+		private SampleProvider usSamples;
+		
+		
+		private final double TRACK = 0;
+		private final double WHEEL_RAD = 0;
+		public static final int MOTOR_SPEED = 250;
+		private static final int ROTATE_SPEED = 150;
+		
+		//Other global variables
+
+		
+		//Odometer initializers
+		private Odometer odometer;
+		private OdometerData odoData;
+		
+		//Enum created to distinguish between whether to start with rising edge or falling edge
+		public enum edgeType {
+			FallingEdge, RisingEdge; 
+		}
+		private edgeType type; 
+		
+		public UltrasonicLocalizer(EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor,
+			final double TRACK, final double WHEEL_RAD, edgeType type) throws OdometerExceptions {
+			
+			//Inheriting references to the motors first
+			this.leftMotor = leftMotor; 
+			this.rightMotor = rightMotor; 
+			
+			//Setting up odometer to 0,0,0
+			this.odometer = Odometer.getOdometer(); 
+			this.odoData = Odometer.getOdometerData(); 
+			odoData.setXYT(0, 0, 0); //the orientation is set according to where robot starts
+			
+			//Setting up the ultrasonic sensor
+			SensorModes usSensor = new EV3UltrasonicSensor(SensorPort.S1);
+			usSamples = usSensor.getMode("Distance"); 
+			this.usData = new float[usSamples.sampleSize()]; 
+			
+			//Setting up whether falling edge or rising edge
+			this.type = type; 
+			
+		}
+		
+		public void run() {
+			//The main threaded method.
+			
+			if(type == edgeType.FallingEdge) {
+				fallingEdge();
+			}
+			else if(type == edgeType.RisingEdge){
+				risingEdge(); 
+			}
+			else {
+				throw new IllegalArgumentException(); 
+			}
+			
+		}
+		
+		public void fallingEdge() {
+			
+		}
+		
+		public void risingEdge() {
+				
+		}
+
+	
 }
