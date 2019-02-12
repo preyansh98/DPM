49a3a0c11d3691aaae7de64639d8c4a20c0ad260
Preyansh <preyansh98@gmail.com>
2019-02-12 10:57:40 -0500

    Demo done, fully functional

--- LocalizationLab/src/ca/mcgill/ecse211/lab4/LightLocalizer.java
+++ LocalizationLab/src/ca/mcgill/ecse211/lab4/LightLocalizer.java
@@ -2,6 +2,7 @@ package ca.mcgill.ecse211.lab4;
 
 
 import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.lcd.LCD;
 import lejos.hardware.lcd.TextLCD;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.Port;
@@ -13,107 +14,248 @@ import lejos.hardware.Sound;
 
 public class LightLocalizer {
 	
-	private final double f = 14; // distance separating the light sensor from the center of the wheels
-	public static int forwardSpeed = 100;
-	private static final double blackValue = 0.3;
-	Odometer odometer;
-	private EV3LargeRegulatedMotor leftMotor, rightMotor;
-	final TextLCD display = LocalEV3.get().getTextLCD();
-	// light sensor setup
-	private static final Port lightPort = LocalEV3.get().getPort("S4");
-	SensorModes lightSensor = new EV3ColorSensor(lightPort);
-	//Samples for light sensor
-	SampleProvider lightValue = lightSensor.getMode("Red"); 
-	float[] lsData = new float[lightValue.sampleSize()]; 
-	
-	
-	public LightLocalizer(EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor,
-	Odometer odometer) {
-	this.odometer = odometer;
-	this.leftMotor = leftMotor;
-	this.rightMotor = rightMotor;
-	}
-	
-	
-	/**
-	 * lightLocalization uses the light sensor to get to the origin, using the lines detected and the distances measured
-	 *  from each line detection. 
-	 **/
-	
-	public void lightLocalize(){
-	
-	//setting speed and moving forward - waiting for black line
-	this.leftMotor.setSpeed(forwardSpeed);
-	this.rightMotor.setSpeed(forwardSpeed);
-	this.leftMotor.setAcceleration(500);
-	this.rightMotor.setAcceleration(500);
-	this.leftMotor.forward();
-	this.rightMotor.forward();
-	
-	//Get sensor values
-	lightValue.fetchSample(lsData, 0);
-	float reading = lsData[0] ; 
-	display.drawString("Color: " + reading, 0, 4);
-	while (reading >= blackValue) {
-	lightValue.fetchSample(lsData, 0);
-	reading = lsData[0] ; //updating value
-	display.drawString(" ", 0, 4);
-	display.drawString("Color: " + reading, 0, 4);
-	}
-	
-	
-	//Stop when a black line is detected
-	this.leftMotor.setSpeed(0);
-	this.rightMotor.setSpeed(0);
-	Sound.systemSound(false, 3);
-	this.leftMotor.setSpeed(forwardSpeed);
-	this.rightMotor.setSpeed(forwardSpeed);
-	
-	
-	// turn clockwise 90deg and continue forward until next line is detected
-	
-	this.leftMotor.rotate(convertAngle(90, Lab4.TRACK, Lab4.WHEEL_RAD), true);
-	this.rightMotor.rotate(-convertAngle(90, Lab4.TRACK, Lab4.WHEEL_RAD), false);
-	this.leftMotor.forward();
-	this.rightMotor.forward();
-	lightValue.fetchSample(lsData, 0);
-	reading = lsData[0]; 
-	display.drawString("Color: " + reading, 0, 4);
-	while (reading >= blackValue) {
-	lightValue.fetchSample(lsData, 0);
-	reading = lsData[0]; 
-	display.drawString("Color: " + reading, 0, 4);
-	}
-	this.leftMotor.setSpeed(0);
-	this.rightMotor.setSpeed(0);
-	Sound.playNote(Sound.FLUTE, 880, 250);
-	this.leftMotor.setSpeed(forwardSpeed);
-	this.rightMotor.setSpeed(forwardSpeed);
-	
-	//rotation
-	this.leftMotor.rotate(convertDistance(f, Lab4.WHEEL_RAD), true);
-	this.rightMotor.rotate(convertDistance(f, Lab4.WHEEL_RAD), false);
-	
-	//counterclockwise rotation for 90 deg
-	this.leftMotor.rotate(-convertAngle(90, Lab4.TRACK, Lab4.WHEEL_RAD), true);
-	this.rightMotor.rotate(convertAngle(90, Lab4.TRACK, Lab4.WHEEL_RAD), false);
-	
-	// move "f" cm  
-	this.leftMotor.rotate(convertDistance(f, Lab4.WHEEL_RAD), true);
-	this.rightMotor.rotate(convertDistance(f, Lab4.WHEEL_RAD), false);
-	
-	// set all values to 0 when at origin
-	this.odometer.setX(0);
-	this.odometer.setY(0);
-	this.odometer.setTheta(0);
-	}
-	
-	private int convertDistance(double distance, double wheelRadius) {
-		return (int) ((int) (180*distance) / (Math.PI*wheelRadius));
-	}	
+	private final double SENSOR_OFFSET = 13; 
 
-	private int convertAngle(double angle, double track, double wheelRadius){
-		return convertDistance(Math.PI*track*angle/360, wheelRadius);
+	private static int ROTATE_SPEED = 50;
+	private static int MOTOR_SPEED = 100; 
+	private static final double lineThreshold = 185; 
+	private int lineCounter = 0; 
+	//Odometer
+	private Odometer odometer; 
+	
+	//Motors
+	private EV3LargeRegulatedMotor leftMotor, rightMotor; 
+	
+	//Setting up light sensor
+	private static final Port lsPort = LocalEV3.get().getPort("S4"); 
+	EV3ColorSensor colSensor = new EV3ColorSensor(lsPort);
+	SampleProvider lsValue; 
+	private float[] lsData; 
+	
+	//other global variables
+	private double TRACK = 0; 
+	private double WHEEL_RAD = 0;  
+	double[] ThetaArray = new double[4];
+	
+	public LightLocalizer(EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor, 
+			Odometer odometer, double TRACK, double WHEEL_RAD) {
+		this.odometer = odometer; 
+		this.leftMotor = leftMotor; 
+		this.rightMotor = rightMotor; 
+		
+		this.TRACK = TRACK; 
+		this.WHEEL_RAD = WHEEL_RAD; 
+		colSensor.setCurrentMode("Red");
+		this.lsValue = colSensor.getMode("Red");
+		this.lsData = new float[lsValue.sampleSize()];
 	}
+	
+	public void mainMethod() {
+		//1st we want it to turn to the origin. 
+		turnTo(45); 
+		
+
+		//Move till the sensor detects origin line 
+		while(getIntensity() > lineThreshold) {
+			leftMotor.setSpeed(MOTOR_SPEED);
+			rightMotor.setSpeed(MOTOR_SPEED);
+			leftMotor.forward();
+			rightMotor.forward();
+			LCD.drawString("value: " + getIntensity(), 0, 5);
+			if(getIntensity() < lineThreshold) {
+				break;
+			}
+		}
+		  leftMotor.stop(true);
+		  rightMotor.stop();
+		  Sound.beep();
+		  
+		//move back by the offset
+		leftMotor.setSpeed(MOTOR_SPEED);
+		rightMotor.setSpeed(MOTOR_SPEED);
+		leftMotor.rotate(convertDistance(WHEEL_RAD, -SENSOR_OFFSET), true);
+		rightMotor.rotate(convertDistance(WHEEL_RAD, -SENSOR_OFFSET), false);
+		
+		//Now advance forward until you read a line
+		
+		while(lineCounter < 4) {
+			turnClockwise(); 
+
+			if(getIntensity() < lineThreshold) {
+				ThetaArray[lineCounter] = odometer.getXYT()[2];
+				Sound.setVolume(10);
+				  Sound.systemSound(false, 4);
+				  lineCounter++;
+			}			
+		}
+
+		/**********************************************************/
+		  double dX, dY, anglex, angley, deltathetaY;
+
+		  // Get our location from origin using the calculated angles
+		  angley = ThetaArray[3] - ThetaArray[1];
+		  anglex = ThetaArray[2] - ThetaArray[0];
+
+		  dX = -SENSOR_OFFSET*Math.cos(Math.toRadians(angley/2)); 
+		  dY = -SENSOR_OFFSET*Math.cos(Math.toRadians(anglex/2));
+		  
+		  deltathetaY = (Math.PI / 2.0) - ThetaArray[3] + Math.PI + (angley / 2.0);
+		  
+		  odometer.setXYT(dX, dY, odometer.getXYT()[2]);
+		  this.travelTo(0.0, 0.0);
+		  do {
+    	  LCD.drawString("X: " +dX, 0, 5);
+    	  LCD.drawString("Y: " + dY, 0, 6);
+		  }
+		  while (isNavigating()); 
+	      
+		  //Now make it turn to 0. 
+		  Sound.setVolume(20);
+		  Sound.systemSound(false, 2);   
+		  
+		  double currentangle = odometer.getXYT()[2]; 
+		  turnTo(-currentangle);
+
+		  
+	}
+	
+	private void turnClockwise() {
+		leftMotor.forward();
+		rightMotor.backward();		
+	}
+	
+	void travelTo(double x, double y) {
+
+		double currentX, currentY, currentT, deltaX, deltaY, dist; 
+		double dTheta = 0; 
+		double position[] = new double[3]; //create a array of size 3 for the odometer
+		position = odometer.getXYT(); // fill the array with the XYT components from odometer
+		currentX = position[0];// get x value at first position of array
+		currentY = position[1];// get y value at second position of array
+		currentT = position[2]; // get theta value at third position of array
+		
+		deltaX = x - currentX; //store difference between waypoint coordinates and odometer coordinates in a variable
+		deltaY = y - currentY; 
+		dist = Math.sqrt(deltaX * deltaX + deltaY*deltaY); //store shortest distance from waypoint in variable
+		
+		//Three cases based on deltaX, deltaY
+		
+		if(deltaY >= 0) {
+			//the robot has to move in positive Y direction to where it is facing
+			
+			//Y is positive, x could be anything because Math.atan calculates from pi/2 to -pi/2
+			//i.e. when Y is positive (quadrants 1 and 2) 
+			
+			dTheta = Math.atan(deltaX/deltaY); //tan is opposite divided by adjacent
+		
+			
+		}
+		else if(deltaY <=0 && deltaX >= 0) {
+			//y is negative, x is positive
+			//quadrant 4
+			
+			dTheta = Math.atan(deltaX/deltaY) + Math.PI; //minimal angle calculation
+			//Math.PI is factored in because the range of atan is from -pi/2 to pi/2
+			//tan(-a) = -tan(a) which is why Math.PI is added not subtracted
+		}
+		else {
+			//y is negative, x is negative.
+			//quadrant 3
+			
+			dTheta = Math.atan(deltaX/deltaY) - Math.PI; 
+		}
+		
+		double thetaDiff = (Math.toDegrees(dTheta)-currentT);
+		//The robot is not always facing 0, so needs to find the difference it needs to turn
+		turnTo(thetaDiff); 
+		
+		leftMotor.setSpeed(250);
+		rightMotor.setSpeed(250);
+		leftMotor.rotate(convertDistance(WHEEL_RAD, dist), true);
+	    rightMotor.rotate(convertDistance(WHEEL_RAD, dist), false); 
+		//convertDistance borrowed from SquareDriver
+	    
+	    
+	
+	}
+
+	private void turnTo(double theta) {		
+		
+		if(theta>180) {
+			
+			/*
+			* If the angle is more than 180, then we need the minimal angle instead, i.e. 360 - angle. 
+			*/
+			theta=360-theta;
+			leftMotor.setSpeed(ROTATE_SPEED);
+			leftMotor.rotate(-convertAngle(WHEEL_RAD, TRACK, theta), true);
+		        rightMotor.setSpeed(ROTATE_SPEED);
+			rightMotor.rotate(convertAngle(WHEEL_RAD, TRACK, theta), false);
+
+			}
+		
+		else if(theta<-180) {
+			
+			/*
+			* If the angle is less than -180, similarly the difference with 360 is needed. 
+			*/
+			theta=360+theta;
+			leftMotor.setSpeed(ROTATE_SPEED);
+			leftMotor.rotate(convertAngle(WHEEL_RAD, TRACK, theta), true);
+			rightMotor.setSpeed(ROTATE_SPEED);
+			rightMotor.rotate(-convertAngle(WHEEL_RAD, TRACK, theta), false);
+		
+		}
+		
+		else {	
+			
+			/*
+			* Angle is within the range so it is already minimal angle. 
+			*/
+			leftMotor.setSpeed(ROTATE_SPEED);
+			leftMotor.rotate(convertAngle(WHEEL_RAD, TRACK, theta), true);
+			rightMotor.setSpeed(ROTATE_SPEED);
+			rightMotor.rotate(-convertAngle(WHEEL_RAD, TRACK, theta), false);
+		
+		}
+	}
+	boolean isNavigating() {
+		//boolean indicates whether travelTo and turnTo are running
+		boolean result = false; 
+		if(leftMotor.isMoving() && rightMotor.isMoving()) {
+			result = true; 
+		}
+		
+		return result; 
+	}
+	
+
+	
+	private double getIntensity() {
+		colSensor.fetchSample(lsData, 0);
+		double lsValue = lsData[0] * 1000; 
+		
+		return lsValue;
+		
+	}
+	
+	  /**
+	   * This method allows the conversion of a distance to the total rotation of each wheel need to
+	   * cover that distance.
+	   * Borrowed from SquareDriver class
+	   * 
+	   * @param radius
+	   * @param distance
+	   * @return an integer tacho count
+	   */
+	  private static int convertDistance(double radius, double distance) {
+	    return (int) ((180.0 * distance) / (Math.PI * radius));
+	  }
+
+	  private static int convertAngle(double radius, double width, double angle) {
+	    return convertDistance(radius, Math.PI * width * angle / 360.0);
+	  }
+
+	  
 
 }
--- LocalizationLab/src/ca/mcgill/ecse211/lab4/UltrasonicLocalizer.java
+++ LocalizationLab/src/ca/mcgill/ecse211/lab4/UltrasonicLocalizer.java
@@ -13,248 +13,252 @@ import lejos.hardware.sensor.SensorModes;
 import lejos.robotics.SampleProvider;
 
 
-/*TODO: 
+/*TODO:
  * 1. Make this class extend the ultrasonic poller and use that instead
  * 2. Make methods for clockwise rotation and anti-clockwise
- * 3. Make method for stopping. 
+ * 3. Make method for stopping.
  */
 public class UltrasonicLocalizer {
-	
-	//Setting up motors
-		private EV3LargeRegulatedMotor leftMotor;
-		private EV3LargeRegulatedMotor rightMotor;
-		
-		//Ultrasonic sensor initializers
-		private float[] usData;
-		private SampleProvider usSamples;
-		
-		//Ultrasonic sensor global variables to process distance
-		private int distance; 
-		private static final int FILTER_OUT = 35;
-		private int filterControl = 0; 
-		
-		//Other global variables
-		private final double TRACK = 0;
-		private final double WHEEL_RAD = 0;
-		private static final int MOTOR_SPEED = 100;
-		private static final int ROTATE_SPEED = 50; //rotate speed significantly lowered to support sample frequency
-		private double deltaT = 0; 
-		
-		private int fallingMin = 18; 
-		private int risingMax = 45; 
-		private int errorMargin = 3;
-		private double TURNING_ERROR = 3.5; 
-		
-		//Ultrasonic Sensor 
-		
-		//Odometer initializers
-		private Odometer odometer;
-		private OdometerData odoData;
-		
-		//Enum created to distinguish between whether to start with rising edge or falling edge
-		public enum edgeType {
-			FallingEdge, RisingEdge; 
-		}
-		
-		private edgeType type; 
-		
-		public UltrasonicLocalizer(EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor,
-			Odometer odometer, final double TRACK, final double WHEEL_RAD, edgeType type,
-			SampleProvider usSamples) throws OdometerExceptions {
-			
-			//Inheriting references to the motors first
-			this.leftMotor = leftMotor; 
-			this.rightMotor = rightMotor; 
-			
-			//Setting up odometer to 0,0,0
-			this.odometer = odometer; 
-			
-			//Setting up whether falling edge or rising edge
-			this.type = type; 
-			
-			//Setting up ultrasonic sensor
-			this.usSamples = usSamples; 
-			this.usData = new float[usSamples.sampleSize()];
-			
-			
-		}
-		
-		public void mainMethod() {
-			//The main threaded method.
-			leftMotor.setSpeed(ROTATE_SPEED);
-			rightMotor.setSpeed(ROTATE_SPEED);
-
-			
-			if(type == edgeType.FallingEdge) {
-				fallingEdge();
-			}
-			else if(type == edgeType.RisingEdge){
-				risingEdge(); 
-			}
-			else {
-				throw new IllegalArgumentException();
-			}
-			
-		}
-		
-		public void fallingEdge() {
-			
-			//1. Rotate to face away from the wall
-			while(getDistance() < fallingMin + errorMargin) {
-				leftMotor.backward();
-				rightMotor.forward();
-				//rotate counter clockwise
-			}
-			
-			//2. Rotate to find the wall
-			while(getDistance() > fallingMin) {
-				leftMotor.backward();
-				rightMotor.forward();
-			}
-			Sound.beep();
-			double firstAngle = odometer.getXYT()[2]; 
-			
-			//3. Rotate outside of the wall now
-			while(getDistance() < fallingMin + errorMargin) {
-				leftMotor.forward();
-				rightMotor.backward();
-			}
-			
-			//4. Find the second wall now 
-			while(getDistance() > fallingMin) {
-				leftMotor.forward();
-				rightMotor.backward();
-			}
-			Sound.beep();
-			double secondAngle = odometer.getXYT()[2]; 
-			
-			leftMotor.stop();
-			rightMotor.stop();
-			
-			if(firstAngle > secondAngle) {
-				deltaT = 225 - (firstAngle + secondAngle)/2; 
-			}
-			else {
-				deltaT = 45 - (firstAngle + secondAngle)/2; 
-			}
-			
-			double turnAngle = deltaT + odometer.getXYT()[2]; 
-			
-			leftMotor.rotate(-convertAngle(Lab4.WHEEL_RAD, Lab4.TRACK, turnAngle-TURNING_ERROR), true);
-			rightMotor.rotate(convertAngle(Lab4.WHEEL_RAD, Lab4.TRACK, turnAngle-TURNING_ERROR), false);
-
-			// set odometer to theta = 0
-			odometer.setXYT(0.0, 0.0, 0.0);
-			 
-		}
-		
-		public void turnTo(double theta) {
-			
-			int rotation = convertAngle(theta, TRACK, WHEEL_RAD);
-			leftMotor.rotate(rotation, true);
-			rightMotor.rotate(-rotation, false);
-			leftMotor.stop();
-			rightMotor.stop();
-		}
-		
-		public void risingEdge() {
-
-			//1. Rotate to the wall 
-			while(getDistance() > fallingMin) {
-				leftMotor.backward();
-				rightMotor.forward();
-			}
-			
-			//2. Rotate until open space
-			while(getDistance() < fallingMin + errorMargin) {
-				leftMotor.backward();
-				rightMotor.forward(); 
-			}
-			Sound.beep();
-			double firstAngle = odometer.getXYT()[2]; 
-			
-			//3. Rotate until sees the wall 
-			while(getDistance() > fallingMin) {
-				leftMotor.forward();
-				rightMotor.backward();
-			}
-			
-			//4. Rotate until open space again
-			while(getDistance() < fallingMin + errorMargin) {
-				leftMotor.forward();
-				rightMotor.backward();
-			}
-			Sound.beep();
-			double secondAngle = odometer.getXYT()[2]; 
-			
-			leftMotor.stop();
-			rightMotor.stop();
-			
-			if(firstAngle > secondAngle) {
-				deltaT = 225 - (firstAngle + secondAngle)/2 + 180; 
-			}
-				else {
-					deltaT = 45 - (firstAngle + secondAngle)/2 + 180; 
-				}
-			
-			double turnAngle = deltaT + odometer.getXYT()[2]; 
-			
-			leftMotor.rotate(-convertAngle(Lab4.WHEEL_RAD, Lab4.TRACK, turnAngle-TURNING_ERROR), true);
-			rightMotor.rotate(convertAngle(Lab4.WHEEL_RAD, Lab4.TRACK, turnAngle-TURNING_ERROR), false);
-
-			// set theta = 0.0
-			odometer.setXYT(0.0, 0.0, 0.0);
-			}
-
-		
-
-		public void processUSData(int distance) {
-			
-			// rudimentary filter - toss out invalid samples corresponding to null
-			// (n.b. this was not included in the Bang-bang controller, but easily
-			// could have).
-			//
-			if (distance >= 255 && filterControl < FILTER_OUT) {
-				// bad value, do not set the distance var, however do increment the
-				// filter value to read continous samples until Filter_out is met
-				filterControl++;
-			} else if (distance >= 255) {
-				// We have repeated large values, so there must actually be nothing
-				// there: leave the distance alone and pass it to the processUSData
-				this.distance = distance;
-			} else {
-				// distance went below 90: reset filter and leave
-				// distance alone.
-				filterControl = 0;
-				this.distance = distance;
-			}
-			LocalEV3.get().getTextLCD().drawString("distance: " + distance, 0, 6);
-			
-			
-		}
-		
-		public int getDistance() {
-			usSamples.fetchSample(usData, 0); 
-			int distance = (int) (usData[0] * 100); 
-			return distance; 
-		}
-
-		  /**
-		   * This method allows the conversion of a distance to the total rotation of each wheel need to
-		   * cover that distance.
-		   * Borrowed from SquareDriver class
-		   * 
-		   * @param radius
-		   * @param distance
-		   * @return an integer tacho count
-		   */
-		  private static int convertDistance(double radius, double distance) {
-		    return (int) ((180.0 * distance) / (Math.PI * radius));
-		  }
-
-		  private static int convertAngle(double radius, double width, double angle) {
-		    return convertDistance(radius, Math.PI * width * angle / 360.0);
-		  }
-
-
+    
+    //Setting up motors
+    private EV3LargeRegulatedMotor leftMotor;
+    private EV3LargeRegulatedMotor rightMotor;
+    
+    //Ultrasonic sensor initializers
+    private float[] usData;
+    private SampleProvider usSamples;
+    
+    //Ultrasonic sensor global variables to process distance
+    private int distance;
+    private static final int FILTER_OUT = 35;
+    private int filterControl = 0;
+    
+    //Other global variables
+    private final double TRACK = 0;
+    private final double WHEEL_RAD = 0;
+    private static final int MOTOR_SPEED = 100;
+    private static final int ROTATE_SPEED = 50; //rotate speed significantly lowered to support sample frequency
+    private double deltaT = 0;
+    
+    private int fallingMin = 18;
+    private int risingMax = 45;
+    private int errorMargin = 3;
+    private double TURNING_ERROR = 3.5;
+    
+    //Ultrasonic Sensor
+    
+    //Odometer initializers
+    private Odometer odometer;
+    private OdometerData odoData;
+    
+    //Enum created to distinguish between whether to start with rising edge or falling edge
+    public enum edgeType {
+        FallingEdge, RisingEdge;
+    }
+    
+    private edgeType type;
+    
+    public UltrasonicLocalizer(EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor,
+                               Odometer odometer, final double TRACK, final double WHEEL_RAD, edgeType type,
+                               SampleProvider usSamples) throws OdometerExceptions {
+        
+        //Inheriting references to the motors first
+        this.leftMotor = leftMotor;
+        this.rightMotor = rightMotor;
+        
+        //Setting up odometer to 0,0,0
+        this.odometer = odometer;
+        
+        //Setting up whether falling edge or rising edge
+        this.type = type;
+        
+        //Setting up ultrasonic sensor
+        this.usSamples = usSamples;
+        this.usData = new float[usSamples.sampleSize()];
+        
+        
+    }
+    
+    public void mainMethod() {
+        //The main threaded method.
+        leftMotor.setSpeed(ROTATE_SPEED);
+        rightMotor.setSpeed(ROTATE_SPEED);
+        
+        
+        if(type == edgeType.FallingEdge) {
+            fallingEdge();
+        }
+        else if(type == edgeType.RisingEdge){
+            risingEdge();
+        }
+        else {
+            throw new IllegalArgumentException();
+        }
+        
+    }
+    
+    public void fallingEdge() {
+        
+        //1. Rotate to face away from the wall
+        while(getDistance() < fallingMin + errorMargin) {
+            turnCounterclockwise();//call method to turn counter-clockwise
+            //rotate counter clockwise
+        }
+        
+        //2. Rotate to find the wall
+        while(getDistance() > fallingMin) {
+            turnCounterclockwise();//call method to turn counter-clockwise
+            
+        }
+        Sound.beep();
+        double firstAngle = odometer.getXYT()[2];
+        
+        //3. Rotate outside of the wall now
+        while(getDistance() < fallingMin + errorMargin) {
+            turnClockwise();//call method to turn clockwise
+        }
+        
+        //4. Find the second wall now
+        while(getDistance() > fallingMin) {
+            turnClockwise();//call method to turn clockwise
+        }
+        Sound.beep();
+        double secondAngle = odometer.getXYT()[2];
+        
+        stop();//call stop method to stop the robot
+        
+        if(firstAngle > secondAngle) {
+            deltaT = 225 - (firstAngle + secondAngle)/2;
+        }
+        else {
+            deltaT = 45 - (firstAngle + secondAngle)/2;
+        }
+        
+        double turnAngle = deltaT + odometer.getXYT()[2];
+        
+        leftMotor.rotate(-convertAngle(Lab4.WHEEL_RAD, Lab4.TRACK, turnAngle-TURNING_ERROR), true);
+        rightMotor.rotate(convertAngle(Lab4.WHEEL_RAD, Lab4.TRACK, turnAngle-TURNING_ERROR), false);
+        
+        // set odometer to theta = 0
+        odometer.setXYT(0.0, 0.0, 0.0);
+        
+    }
+    
+    public void turnTo(double theta) {
+        
+        int rotation = convertAngle(theta, TRACK, WHEEL_RAD);
+        leftMotor.rotate(rotation, true);
+        rightMotor.rotate(-rotation, false);
+        stop();//call method to stop robot
+    }
+    
+    public void risingEdge() {
+        
+        //1. Rotate to the wall
+        while(getDistance() > fallingMin) {
+            turnCounterclockwise();//call method to turn counter-clockwise
+        }
+        
+        //2. Rotate until open space
+        while(getDistance() < fallingMin + errorMargin) {
+            turnCounterclockwise();//call method to turn counter-clockwise
+        }
+        Sound.beep();
+        double firstAngle = odometer.getXYT()[2];
+        
+        //3. Rotate until sees the wall
+        while(getDistance() > fallingMin) {
+            turnClockwise();//call method to turn clockwise
+        }
+        
+        //4. Rotate until open space again
+        while(getDistance() < fallingMin + errorMargin) {
+            turnClockwise();//call method to turn clockwise
+        }
+        Sound.beep();
+        double secondAngle = odometer.getXYT()[2];
+        
+        stop();//stop robot
+        
+        if(firstAngle > secondAngle) {
+            deltaT = 225 - (firstAngle + secondAngle)/2 + 180;
+        }
+        else {
+            deltaT = 45 - (firstAngle + secondAngle)/2 + 180;
+        }
+        
+        double turnAngle = deltaT + odometer.getXYT()[2];
+        
+        leftMotor.rotate(-convertAngle(Lab4.WHEEL_RAD, Lab4.TRACK, turnAngle-TURNING_ERROR), true);
+        rightMotor.rotate(convertAngle(Lab4.WHEEL_RAD, Lab4.TRACK, turnAngle-TURNING_ERROR), false);
+        
+        // set theta = 0.0
+        odometer.setXYT(0.0, 0.0, 0.0);
+    }
+    
+    
+    
+    public void processUSData(int distance) {
+        
+        // rudimentary filter - toss out invalid samples corresponding to null
+        // (n.b. this was not included in the Bang-bang controller, but easily
+        // could have).
+        //
+        if (distance >= 255 && filterControl < FILTER_OUT) {
+            // bad value, do not set the distance var, however do increment the
+            // filter value to read continous samples until Filter_out is met
+            filterControl++;
+        } else if (distance >= 255) {
+            // We have repeated large values, so there must actually be nothing
+            // there: leave the distance alone and pass it to the processUSData
+            this.distance = distance;
+        } else {
+            // distance went below 90: reset filter and leave
+            // distance alone.
+            filterControl = 0;
+            this.distance = distance;
+        }
+        LocalEV3.get().getTextLCD().drawString("distance: " + distance, 0, 6);
+        
+        
+    }
+    
+    public int getDistance() {
+        usSamples.fetchSample(usData, 0);
+        int distance = (int) (usData[0] * 100);
+        return distance;
+    }
+    
+    /**
+     * This method allows the conversion of a distance to the total rotation of each wheel need to
+     * cover that distance.
+     * Borrowed from SquareDriver class
+     *
+     * @param radius
+     * @param distance
+     * @return an integer tacho count
+     */
+    private static int convertDistance(double radius, double distance) {
+        return (int) ((180.0 * distance) / (Math.PI * radius));
+    }
+    
+    private static int convertAngle(double radius, double width, double angle) {
+        return convertDistance(radius, Math.PI * width * angle / 360.0);
+    }
+    
+    public void turnClockwise() {
+        leftMotor.forward();
+        rightMotor.backward();
+    }
+    
+    public void turnCounterclockwise() {
+        leftMotor.backward();
+        rightMotor.forward();
+    }
+    
+    public void stop() {
+        leftMotor.stop();
+        rightMotor.stop();
+    }
+    
 }
